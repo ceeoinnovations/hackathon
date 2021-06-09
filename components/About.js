3fdd02e50ad18b1c10eac5ac13c3719ca4822e4d
hyejinim <hyejinim17@gmail.com>
2021-06-09 13:37:31 -0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -4,6 +4,7 @@ export default function About(about){
         <div class="text-wrapper">
         <br><br>
                  <h1 class="title">${about[0].name}</h1>
+                 <img src="${about[0].teaser}" div class="teaser">
                  <p>${about[0].description}</p>
         </div >    
     </section>`
