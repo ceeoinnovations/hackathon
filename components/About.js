7c30a2d007a6d7e6e3aad1859497da9206e7f25e
Hyejin Im <hyejinim17@gmail.com>
2021-06-10 09:59:08 -0400

    Update files

--- components/About.js
+++ components/About.js
@@ -3,13 +3,15 @@ export default function About(about){
     <section id="about" class="intro">
         <div class="text-wrapper">
         <br><br>
-                 <h1 class="title">${about[0].name}</h1>
-                 <p>${about[0].description}</p>
-                 <div class="project-img">
-                    <img src="${about[0].image1}" div class="project-img-holder">
-                    <img src="${about[0].image2}" div class="project-img-holder">
-                 </div>
-                 <button class="button" style="margin-top: 40px; margin-bottom: 100px;"><a href="${about[0].buttonlink}" target="_blank">${about[0].buttonlabel}</a></button>
+            <h1 class="title">${about[0].name}</h1>
+            <p>${about[0].description}</p>
+            <div class="project-img">
+            <img src="${about[0].image1}" div class="project-img-holder">
+            <img src="${about[0].image2}" div class="project-img-holder">
+            </div>
+            <a href="${about[0].buttonlink}" target="_blank">
+                <button class="button" style="margin-top: 40px; margin-bottom: 100px;">${about[0].buttonlabel}</button>
+            </a>
         </div >    
     </section>`
 }
